6cd3316418a82abcf03be38bfb0ed9a80f9e1ecb
Brayden <braydenparker2701@gmail.com>
2025-12-24 20:42:31 +1100

    started c++ solution and added gitignore

---
+++ solutions/day1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::string line;
+    int turn;
+    int passes;
+    int position = 50;
+
+    std::ifstream file("../inputs/day1example.txt");
+
+    while (getline (file, line)) {
+        turn = std::stoi(line.substr(1));
+
+        while(turn > 0){
+            if (line[0] == 'L'){
+                position = (position + 99) % 100;
+                turn--;
+            } else {
+                position = (position + 1) % 100;
+                turn--;
+            }
+            if (position == 0){
+                std::cout<< line.substr(1) << std::endl;
+                passes++;
+            }
+        }
+        //std::cout << "Position " << position << std::endl;
+
+    }
+
+    //std::cout << "Passes: " << passes;
+
+    //std::cout << "Position: " << position;
+
+    std::cout << "\n\n";
+
+
+        //for line in input:
+        //for i in range(int(line[1:])):
+        //    if line[0] == 'L':
+        //        position = (position + 100 - 1) % 100
+        //    else:
+        //        position = (position + 1) % 100
+        //    if position == 0:
+        //        passes += 1
+
+    return 0;
+}
